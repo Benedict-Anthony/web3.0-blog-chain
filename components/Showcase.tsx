fba640a2ca6d031a48c4b98382ffe107d63ab04f
Benedict Anthony <benedict.ughulu@gmail.com>
2022-12-23 22:57:57 +0100

    Update Showcase.tsx

--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -15,7 +15,7 @@ const Showcase = () => {
                       {/* <img src={} alt=""  /> */}
                   </div>
                   <article className={ShowCaseStyles.article}>
-                      <h1>The Bright <br /> Future of <br /> Web.0?</h1>
+                      <h1>The Bright <br /> Future of <br /> Web 3.0?</h1>
                       <div>
                           <p>  We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
                 But is it really fulfilling its promise?</p>
